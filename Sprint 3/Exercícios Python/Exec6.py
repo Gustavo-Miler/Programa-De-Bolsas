f1faf2ef2fa4e79fd9cd696c86d0cc2cbf93c7a9
unknown <gustavo.miler@live.com>
2023-02-23 10:49:37 -0300

    Renomeando arquivos

--- Sprint 3/Exercícios Python/Exec6.py
+++
@@ -1,8 +0,0 @@
-#Escreva um programa que retorne o que ambas as listas têm em comum (sem repetições). 
-#O seu programa deve funcionar para listas de qualquer tamanho.
-
-a = [1, 1, 2, 3, 5, 8, 14, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-c = list(set(num for num in a if num in b))
-print(c)
