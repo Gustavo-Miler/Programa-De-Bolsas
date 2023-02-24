9226e9b33a2f8074cd60fa2a46dc994b5c47e45d
Gustavo Miler <gustavo.miler@live.com>
2023-02-24 13:31:37 -0300

    consertando print

--- Sprint 3/Exercícios Python/Exec11.py
+++ Sprint 3/Exercícios Python/Exec11.py
@@ -1,4 +1,4 @@
 #Escreva um programa que lê o conteúdo do arquivo texto arquivo_texto.txt e imprime o seu conteúdo.
 
 with open('arquivo_texto.txt') as arquivo:
-    print(arquivo.read())
+    print(arquivo.read(), end="")
