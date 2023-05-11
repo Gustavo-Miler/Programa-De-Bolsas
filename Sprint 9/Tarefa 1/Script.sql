27d4b7bc800c1df2643a320d949b73cd7b818867
unknown <gustavo.miler@live.com>
2023-05-11 20:29:05 -0300

    atualizando tarefa 1

--- Sprint 9/Tarefa 1/Script.sql
+++ Sprint 9/Tarefa 1/Script.sql
@@ -1,57 +1,121 @@
-ALTER TABLE tb_locacao RENAME TO tb_geral
+-- alterando nome de tb_locacao para tb_geral (melhor leitura)
 
-CREATE TABLE tb_cliente AS
+ALTER TABLE tb_locacao 
+RENAME TO tb_geral;
+
+-- criando tabela cliente
+
+CREATE TABLE tb_cliente (
+	id     INT,
+	nome   VARCHAR(15),
+	cidade VARCHAR(40),
+	estado VARCHAR(40),
+	pais   VARCHAR(40)
+)
+
+INSERT INTO tb_cliente
 SELECT DISTINCT
-	idCliente     AS id,
-	nomeCliente   AS nome,
-	cidadeCliente AS cidade,
-	estadoCliente AS estado,
-	paisCliente   AS pais
+	idCliente,
+	nomeCliente,
+	cidadeCliente,
+	estadoCliente,
+	paisCliente
 FROM tb_geral
-ORDER BY id
+ORDER BY idCliente
 
-CREATE TABLE tb_carro AS
-SELECT DISTINCT 
-	idCarro       AS id,
-	classiCarro   AS classi,
-	marcaCarro    AS marca,
-	modeloCarro   AS modelo,
-	anoCarro      AS ano,
-	idcombustivel AS idCombustivel
-FROM tb_geral
-ORDER BY id
+-- criando tabela vendedor
 
-CREATE TABLE tb_combustivel AS
+CREATE TABLE tb_vendedor (
+	id     INT,
+	nome   VARCHAR(15),
+	sexo   SMALLINT,
+	estado VARCHAR(40)
+)
+
+INSERT INTO tb_vendedor
 SELECT DISTINCT
-	idcombustivel   AS id,
-	tipoCombustivel AS tipo
-FROM tb_geral 
-ORDER BY id
-
-CREATE TABLE tb_vendedor AS
-SELECT DISTINCT
-	idVendedor     AS id,
-	nomeVendedor   AS nome,
-	sexoVendedor   AS sexo,
-	estadoVendedor AS estado
+	idVendedor,
+	nomeVendedor,
+	sexoVendedor,
+	estadoVendedor
 FROM tb_geral
-ORDER BY id
+ORDER BY idVendedor
 
-CREATE TABLE tb_locacao AS
+-- criando tabela combustivel
+
+CREATE TABLE tb_combustivel (
+	id   INT,
+	tipo VARCHAR(20)
+)
+
+INSERT INTO tb_combustivel
+SELECT DISTINCT
+	idcombustivel,
+	tipoCombustivel
+FROM tb_geral
+ORDER BY idcombustivel
+
+-- criando tabela carro
+
+CREATE TABLE tb_carro (
+	id            INT,
+	kmAtual       INT,
+	classi        VARCHAR(50),
+	marca         VARCHAR(80),
+	modelo        VARCHAR(80),
+	ano           INT,
+	idCombustivel INT,
+	FOREIGN KEY (idCombustivel) REFERENCES tb_combustivel(id)
+)
+
+INSERT INTO tb_carro
+SELECT DISTINCT
+	idCarro,
+	max(kmCarro),
+	classiCarro,
+	marcaCarro,
+	modeloCarro,
+	anoCarro,
+	idcombustivel
+FROM tb_geral
+GROUP BY idCarro
+ORDER BY idCarro
+
+-- criando tabela locacao
+
+CREATE TABLE tb_locacao (
+	id          INT,
+	idCliente   INT,
+	idCarro     INT,
+	kmCarro     INT,
+	dataLocacao DATE,
+	horaLocacao TIME,
+	qtdDiaria   INT,
+	vlrDiaria   DECIMAL(18,2),
+	dataEntrega DATE,
+	horaEntrega TIME,
+	idVendedor  INT,
+	FOREIGN KEY (idCliente)  REFERENCES tb_cliente(id),
+	FOREIGN KEY (idCarro)    REFERENCES tb_carro(id),
+	FOREIGN KEY (idVendedor) REFERENCES tb_vendedor(id)
+)
+
+INSERT INTO tb_locacao
 SELECT
-	idLocacao AS id,
+	idLocacao,
 	idCliente,
 	idCarro,
-	kmCarro   AS km,
+	kmCarro,
 	dataLocacao,
 	horaLocacao,
-	qtdDiaria AS dias,
-	vlrDiaria AS valorDiaria,
+	qtdDiaria,
+	vlrDiaria,
 	dataEntrega,
 	horaEntrega,
 	idVendedor
 FROM tb_geral
-ORDER BY id
+
+-- Visualização das tabelas
 
 SELECT * FROM tb_geral;
 
@@ -59,8 +123,8 @@ SELECT * FROM tb_locacao;
 
 SELECT * FROM tb_cliente;
 
+SELECT * FROM tb_vendedor;
+
 SELECT * FROM tb_carro;
 
 SELECT * FROM tb_combustivel;
-
-SELECT * FROM tb_vendedor;
